27417cae3483abe907a9a30a71176b5d2724251f
yj6662 <yj6662@naver.com>
2025-06-10 14:28:49 +0900

    [feat] Player
    -feat Player 플레이어 정보 분리

--- YJ_3D_IDLE_Project/Assets/Scripts/Player.cs
+++ YJ_3D_IDLE_Project/Assets/Scripts/Player.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ShipStatsData baseStats;
+
+    public float maxHp;
+    public float currentHp;
+    public float baseAttackPower;
+    public float moveSpeed;
+
+    private float attackBuffMultiplier = 1.0f;
+
+    public float CurrentFinalAttackPower => baseAttackPower * attackBuffMultiplier;
+
+    private void Awake()
     {
-        
+        if (baseStats == null)
+        {
+            maxHp = baseStats.maxHp;
+            currentHp = maxHp;
+            baseAttackPower = baseStats.attackPower;
+            moveSpeed = baseStats.moveSpeed;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float damage)
     {
-        
+        currentHp -= damage;
+        if (currentHp <= 0)
+        {
+            currentHp = 0;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Time.timeScale = 0;
+        //TODO: 게임 오버 처리
+    }
+
+    public void UpgradeAttack(float amount)
+    {
+        baseAttackPower += amount;
+    }
+
+    public IEnumerator ApplyAttackBuff(float amount, float duration)
+    {
+        attackBuffMultiplier = amount;
+
+        yield return new WaitForSeconds(duration);
+        attackBuffMultiplier = 1.0f;
     }
 }
+
